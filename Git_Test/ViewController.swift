937458b9ccb30898651d2896505b859534e1efbe
Ethan Grace-Platow <Ethan@Ethans-MacBook-Pro.local>
2017-05-18 10:31:44 +1000

    Initial commit

---
+++ Git_Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git_Test
+//
+//  Created by Ethan Grace-Platow on 18/5/17.
+//  Copyright © 2017 Ethan Grace-Platow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
